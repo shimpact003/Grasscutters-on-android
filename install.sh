8ed21230b087f461893ac65a235421efd8385636
shimpact003 <42101670+shimpact003@users.noreply.github.com>
2023-12-28 18:23:06 +0000

    Don't pull commit history of GC-Resource

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 pkg install tur-repo -y && yes y|pkg upgrade && pkg install mongodb openjdk-17 python-pip rust binutils wget curl -y
 CFLAGS="-Wno-incompatible-function-pointer-types" pip install mitmproxy
 wget https://github.com/Grasscutters/Grasscutter/releases/download/v1.7.4/grasscutter-1.7.4.jar
-git clone https://gitlab.com/YuukiPS/GC-Resources
+git clone --depth 1 https://gitlab.com/YuukiPS/GC-Resources
 java -jar grasscutter-1.7.4.jar
 cd GC-Resources/Resources
 mv BinOutput/ ExcelBinOutput/ Scripts ScriptSceneData/ Server TextMap/ ~/Grasscutters-on-android/resources
