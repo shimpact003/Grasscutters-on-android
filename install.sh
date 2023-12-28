136a207781bd079fd5d5156942dda191f15e86b3
shimpact003 <42101670+shimpact003@users.noreply.github.com>
2023-12-28 18:10:59 +0000

    Don't prompt during installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pkg install tur-repo -y && yes y|pkg upgrade && pkg install mongodb openjdk-17 python-pip rust binutils wget curl
+pkg install tur-repo -y && yes y|pkg upgrade && pkg install mongodb openjdk-17 python-pip rust binutils wget curl -y
 CFLAGS="-Wno-incompatible-function-pointer-types" pip install mitmproxy
 wget https://github.com/Grasscutters/Grasscutter/releases/download/v1.7.4/grasscutter-1.7.4.jar
 git clone https://gitlab.com/YuukiPS/GC-Resources
